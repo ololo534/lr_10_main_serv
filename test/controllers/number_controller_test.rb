e34db7799f0e54ebcbd1f543c5bac417dd234407
Ivan <vanekkravtsov@mail.ru>
2019-12-01 16:44:09 +0300

    final commit

--- test/controllers/number_controller_test.rb
+++ test/controllers/number_controller_test.rb
@@ -8,23 +8,18 @@ class NumberControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
   end
 
-  test 'should get output' do
-    get number_output_url
-    assert_response :success
-  end
-
   test '0 for negative' do
-    get number_output_url, params: { value: -1}
-    assert_equal assigns[:result].length, 1
+    get result_path, params: { value: -1}
+    assert_equal assigns[:result].length, 0
   end
 
   test '0 for string' do
-    get number_output_url, params: {value: 'Hello, World!'}
-    assert_equal assigns[:result].length, 1
+    get result_path, params: {value: 'Hello, World!'}
+    assert_equal assigns[:result].length, 0
   end
 
   test 'normal data' do
-    get number_output_url, params: {value: 5}
+    get result_path, params: {value: 5}
     assert_equal assigns[:result].length, 3
   end
 end
